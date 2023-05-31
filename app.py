c62ba287d344340baed4a576fb04daa518ca2d45
hessdale <hessdale96@gmail.com>
2023-05-30 19:04:05 -0600

    messing around with python venv

---
+++ app.py
@@ -0,0 +1,9 @@
+import random
+
+getnum=random.randint(0,100)
+
+if getnum >= 50:
+    print('winner')
+elif getnum <= 50:
+    print('loser')
+    
---
+++ helpers/log.py
@@ -0,0 +1,2 @@
+def log(to_print)
+    print('logging: 'to_print)
